1411936b1da8c142beac0352a08fde936e0b2344
Vivian stanley <vivianstanley@Vivians-MacBook-Air.local>
2021-07-25 11:56:12 -0700

    commit tree c806ac34d0f6eae013e3275c3287f07e20395479

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -20,4 +20,16 @@ class SessionsController < ApplicationController
     # Ye olde redirect
     redirect_to root_path
   end
+
+  def create
+    user = User.find_or_create_by(uid: auth['uid']) do |u|
+      u.name = auth['info']['name']
+
+    end
+    session[:user_id] = user.id
+  end
+
+  def auth
+    request.env['omniauth.auth']
+  end
 end
